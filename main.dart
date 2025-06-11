0b4ad01d2d4c96a9f94e296b8e518fae4be8600e
AREEBA-13 <aareebaarif32@gmail.com>
2025-06-11 18:56:07 +0500

    day4 completed

--- main.dart
+++ main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/pages/home_page.dart';
 import 'package:flutter_application_1/pages/login_page.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(MyApp());
@@ -13,12 +14,15 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       themeMode: ThemeMode.light,
-      theme: ThemeData(useMaterial3: false, primarySwatch: Colors.red),
+      theme: ThemeData(
+        useMaterial3: false,
+        primarySwatch: Colors.red,
+        fontFamily: GoogleFonts.lato().fontFamily,
+      ),
       darkTheme: ThemeData(
         brightness: Brightness.dark,
         primarySwatch: Colors.red,
       ),
-      initialRoute: "/home",
       routes: {
         "/": (context) => const LoginPage(),
         "/home": (context) => const HomePage(),
--- pages/login_page.dart
+++ pages/login_page.dart
@@ -4,15 +4,49 @@ class LoginPage extends StatelessWidget {
   const LoginPage({super.key});
   @override
   Widget build(BuildContext context) {
-    return Material(child:
-     Center(child:
-      Text("login page",style: TextStyle(
-        fontSize: 30,
-        color: Color.fromRGBO(228, 36, 228, 1),
-        fontWeight: FontWeight.bold,
+    return Material(
+      color: Color.fromRGBO(240, 240, 240, 1),
+      child: Column(
+        children: [
+          Image.asset("assets/images/login_image.png", width: 250, height: 200),
+          SizedBox(height: 10.0),
+          Text(
+            "WELCOME",
+            style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+          ),
+          SizedBox(height: 10.0),
+          Padding(
+            padding: const EdgeInsets.symmetric(
+              vertical: 16.0,
+              horizontal: 32.0,
+            ),
+            child: Column(
+              children: [
+                TextFormField(
+                  decoration: InputDecoration(
+                    labelText: "Username",
+                    hintText: " Enter Username",
+                  ),
+                ),
+                TextFormField(
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    labelText: "Password",
+                    hintText: " Enter Password",
+                  ),
+                ),
+                SizedBox(height: 30.0),
+                ElevatedButton(
+                  onPressed: () {
+                    print("hellow baby");
+                  },
+                  child: Text("LOGIN"),
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
-      ),
-      ),
-      );
+    );
   }
 }
