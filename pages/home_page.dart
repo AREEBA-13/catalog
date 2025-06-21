b14bc4c51854763a348d3f8401c5594f15eac647
AREEBA-13 <aareebaarif32@gmail.com>
2025-06-21 13:30:21 +0500

    day 11 completed

--- pages/home_page.dart
+++ pages/home_page.dart
@@ -1,16 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/models/catalog.dart';
 import 'package:flutter_application_1/widgets/drawer.dart';
+import 'package:flutter_application_1/widgets/item_widget.dart';
 
 class HomePage extends StatelessWidget {
-  final String name = "Tasty Track";
-
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text("Home Page"),),
-      body: Center(child: Text("This is $name")),
+      appBar: AppBar(title: const Text("Home Page")),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: ListView.builder(
+          itemCount: CatalogModel.items.length,
+          itemBuilder: (context, index) {
+            return ItemWidget(item: CatalogModel.items[index]);
+          },
+        ),
+      ),
       drawer: MyDrawer(),
     );
   }
