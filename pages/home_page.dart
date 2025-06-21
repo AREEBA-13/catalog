99bb8d957587c02ba2b27fd9cba694d8905395c5
AREEBA-13 <aareebaarif32@gmail.com>
2025-06-21 14:17:04 +0500

    day 13 completed

--- pages/home_page.dart
+++ pages/home_page.dart
@@ -1,24 +1,44 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'dart:convert';
 import 'package:flutter_application_1/models/catalog.dart';
 import 'package:flutter_application_1/widgets/drawer.dart';
 import 'package:flutter_application_1/widgets/item_widget.dart';
 
-//dummy list generated from CatalogModel
+//catalog.json is loaded from assets/files/catalog.json
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  void initState() {
+    super.initState();
+    loadData();
+    // You can perform any initialization here if needed
+  }
+
+  Future<void> loadData() async {
+    var catalogJson = await rootBundle.loadString("assets/files/catalog.json");
+    var decodedData = jsonDecode(catalogJson);
+    var productsData = decodedData["products"];
+    print(productsData);
+  }
 
   @override
   Widget build(BuildContext context) {
-    final DummyList = List.generate(100, (index) => CatalogModel.items[0]);
+    final dummyList = List.generate(100, (index) => CatalogModel.items[0]);
     return Scaffold(
       appBar: AppBar(title: const Text("Home Page")),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: ListView.builder(
-          itemCount: DummyList.length,
+          itemCount: dummyList.length,
           itemBuilder: (context, index) {
-            return ItemWidget(item: DummyList[index]);
+            return ItemWidget(item: dummyList[index]);
           },
         ),
       ),
