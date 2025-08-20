6f5aaabc0dde6c76c1c3776f60298e8ed0db24ab
Areeba Arif <areebaarif068@gmail.com>
2025-08-20 19:37:56 +0500

    Delete utils directory

--- utils/routes.dart
+++
@@ -1,6 +0,0 @@
-class MyRoutes {
-  static String homeRoute = "/home";
-  static String homeDetailRoute = "/details";
-  static String cartRoute = "/cart";
-  static String loginRoute = "/login";
-}
